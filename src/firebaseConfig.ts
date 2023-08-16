d608f4b6afd8fbee3c439d9149a1dda695f3dc01
vermalove <lvverma4@gmail.com>
2023-08-16 20:42:28 +0530

    Firebase config file

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAQeLehBi199bjM2nW0MPpI2kkJ66D9YLY",
+  authDomain: "auth-team-7687b.firebaseapp.com",
+  projectId: "auth-team-7687b",
+  storageBucket: "auth-team-7687b.appspot.com",
+  messagingSenderId: "569931973861",
+  appId: "1:569931973861:web:1e92f1880e17e185e2800c",
+};
+
+export default firebaseConfig;
