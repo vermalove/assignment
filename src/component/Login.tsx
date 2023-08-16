ebf968cf8472cab7072db31dccc431aa6e53af3b
vermalove <lvverma4@gmail.com>
2023-08-16 20:44:31 +0530

    Add login

---
+++ src/component/Login.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import "./index.css";
+
+const Login: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const handleLogin = async () => {
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+
+      navigate("/Home", { replace: true });
+    } catch (error) {}
+  };
+
+  const handleSignUp = () => {
+    navigate("/Signup", { replace: true });
+  };
+  return (
+    <div className="loginContainer">
+      <div>
+        <h2>Login</h2>
+      </div>
+      <div>
+        <input
+          className="loginInput"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          className="loginInput"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div className="loginBtn">
+        <button onClick={handleLogin}>Login</button>
+
+        <button onClick={handleSignUp}>Sign Up</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
